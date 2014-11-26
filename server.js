b05dc49f7519d4788f8e72b8454afe1ac7811db6
haddada <haddadaseif@gmail.com>
2014-11-26 15:23:02 +0100

    sample test

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 // Requires meanio
 var mean = require('meanio');
 
-// Creates and serves mean application
+// Creates and serves mean application 
 mean.serve({ /*options placeholder*/ }, function(app, config) {
 	var port = config.https && config.https.port ? config.https.port : config.http.port;
 	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
 });
+
+
+//simple test
